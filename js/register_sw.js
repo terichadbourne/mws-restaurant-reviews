41d54bb37d203586d317da63882554940eb6a492
terichadbourne <terichad@gmail.com>
2018-09-22 16:05:41 -0400

    Register service worker
    - Add `js/register_sw.js` to be called from both html files to
    register a service worker `sw.js` (not yet added)

---
+++ js/register_sw.js
@@ -0,0 +1,11 @@
+// if browser supports service worker, register the `sw.js` file
+// console log success or failure 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(success => {
+      console.log("Successfully registered service worker: " + success.scope)
+    })
+    .catch(error => {
+      console.log("Error registering service worker: " + error)
+    })
+}
